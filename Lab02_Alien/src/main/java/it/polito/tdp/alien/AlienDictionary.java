3cc1dbe54e85d703f7ec8cae9dcfac5368cd2fd2
LilCinta <LilCinta@mbp-di-nicola-2.lan>
2020-04-27 17:10:33 +0200

    Implementazione controller

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
@@ -1,5 +1,42 @@
 package it.polito.tdp.alien;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlienDictionary {
+	
+	private List<Word> dictionary;
+
+	/**
+	 * @param dictionary
+	 */
+	public AlienDictionary() {
+		
+		this.dictionary =  new ArrayList<Word>();
+	}
+	
+	public void addWord(String alienWord, String translation) {
+		Word word = new Word(alienWord, translation);
+		if(this.dictionary.contains(word)) {
+			this.dictionary.get(this.dictionary.indexOf(word)).setTranslation(translation);
+			return;
+		}
+		this.dictionary.add(word);
+	}
+	
+	public void resetDictionary() {
+		this.dictionary.clear();
+	}
+	
+	
+	public String translateWord(String alienWord) {
+		Word w = new Word( alienWord);
+		if(this.dictionary.contains(w))
+			return this.dictionary.get(this.dictionary.indexOf(w)).getTranslation();
+		return null;
+				
+	}
+	
+	
 
 }
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -3,6 +3,8 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,6 +12,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private AlienDictionary alienDictionary;
 
     @FXML
     private ResourceBundle resources;
@@ -36,6 +40,57 @@ public class FXMLController {
 
     @FXML
     void doTranslate(ActionEvent event) {
+    	txtResult.clear();
+    	String riga = txtInserisci.getText().toLowerCase();
+    	
+    	// controllo input
+    	
+    	if(riga == null || riga.length()==0) {
+    		txtResult.setText("Inserire una o due parole!");
+    		return;
+    	}
+    	
+    	StringTokenizer st = new StringTokenizer(riga, " ");
+    	
+    	// estraggo la prima parola
+    	
+    	String alienWord = st.nextToken();
+        
+    	if(st.hasMoreElements()) {
+    		// devo inserire sia parola che traduzione nel dizionario
+    		
+    		// Estraggo la seconda parola 
+    		String translation = st.nextToken();
+    		
+    		if(!alienWord.matches("[a-zA-Z]*")  || !translation.matches("[a-zA-Z]*")) {
+    			txtResult.setText("Inserire solo caratteri alfabetici!");
+    			return;
+    		}
+    		
+    		// le parole sono entrambe valide, le posso aggiungere
+    		this.alienDictionary.addWord(alienWord, translation);
+    		
+    		txtResult.setText("La parola: "+alienWord+" è stata inserita nel vocabolario, con traduzione : "+translation);
+    	
+    	} else {    // se invece viene inserita soltanto la parola da tradurre
+    		
+    		// anche qui controllo l input 
+    		if (!alienWord.matches("[a-zA-Z]*")) {
+				txtResult.setText("Inserire solo caratteri alfabetici.");
+				return;
+			}
+    		String translation = this.alienDictionary.translateWord(alienWord);
+    		
+    		if(translation != null) {
+    			txtResult.setText(translation);
+    		} else {
+    			txtResult.setText("La parola cercata non esiste nel dizionario");
+    		}
+    		
+    	}  
+    	
+    	
+    		
 
     }
 
@@ -45,6 +100,8 @@ public class FXMLController {
         assert txtInserisci != null : "fx:id=\"txtInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+         
 
     }
+    
 }
